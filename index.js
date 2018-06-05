a0b0f4e5ab077f599611d0c545ae003a91b96e38
3110592847@qq.com <3110592847@qq.com>
2018-06-04 22:33:13 -0400

    change files

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var dev = function(){
-    var temp = new Date();
-    return temp;
+var arr = [6,5,4,3,2,1];
+var ret = bubbleSort(arr);
+
+var bubbleSort = function(arr){
+    var len = arr.length;
+    for(var i =0;i<len;i++){
+        for(var j=0;j<len-1-i;j++){
+            if(arr[j] > arr[j+1]){
+                var temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
 }
