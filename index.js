0282b75dab02d8f0ace59f0744876cc68e5b4f13
3110592847@qq.com <3110592847@qq.com>
2018-06-07 05:59:43 -0400

    change files

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-(function () {
-    var bubbleSort = function (arr) {
-        var len = arr.length;
-        for (var i = 0; i < len; i++) {
-            for (var j = 0; j < len - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    var temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
+$(document).ready(function () {
+    Vue.component('bar-item', {
+        props: ['item'],
+        template: "<div :id='item.id' class='bar' :style='{height:item.height}'><span>{{item.text}}</span></div>"
+    });
+    var app = new Vue({
+        el: '.graph',
+        data: {
+            bars: [
+                { id: 90, text: 90, height: 90 },
+                { id: 80, text: 80, height: 80 },
+                { id: 70, text: 70, height: 70 },
+                { id: 60, text: 60, height: 60 },
+                { id: 50, text: 50, height: 50 },
+                { id: 40, text: 40, height: 40 },
+                { id: 30, text: 30, height: 30 },
+                { id: 20, text: 20, height: 20 },
+                { id: 10, text: 10, height: 10 }
+            ]
+        }
+    });
+
+    window.app = app;
+    $('.bar').bind('click',function(){
+        var bubbleSort = function (arr) {
+            var len = arr.length;
+            for (var i = 0; i < len; i++) {
+                for (var j = 0; j < len - 1 - i; j++) {
+                    if (arr[j].id > arr[j + 1].id) {
+                        var temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
                 }
             }
-        }
-        return arr;
-    }
-    var arr = [6, 5, 4, 3, 2, 1];
-    var ret = bubbleSort(arr);
-})();
+            return arr;
+        };
+        window.app.$data.bars.sort(function(a,b){ return a.id - b.id > 0; });
+    });
+});
